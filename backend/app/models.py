cd8012c648acf6a1fb1e3e7966f5fd9ea4bd5028
vishalraj9102 <vishalrajmehra95@gmail.com>
2025-04-16 00:05:54 +0530

    fixing db model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,8 @@
- 
 from app import db
 
 class Contact(db.Model):
+    __tablename__ = 'contact_form'  
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
