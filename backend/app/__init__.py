9a385bfb57b1e736a7e1c29c9bec737be8a0a317
vishalraj9102 <vishalrajmehra95@gmail.com>
2025-04-15 18:19:28 +0530

    fixing bug

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_migrate import Migrate  # Import Migrate
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-migrate = Migrate()  # Initialize Migrate
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('../config.py')
-    
+
     CORS(app)
     db.init_app(app)
-    migrate.init_app(app, db)  # Link Migrate to app and db
+    migrate.init_app(app, db)
 
     from app.routes import main
     app.register_blueprint(main)
